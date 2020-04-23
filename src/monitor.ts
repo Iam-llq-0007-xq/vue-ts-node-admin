66a3978d7bb83e44b4157fb44b1c0bdf871230fa
chenxiaoqing_vendor <chenxiaoqing_vendor@sensetime.com>
2020-04-23 18:17:53 +0800

    feat:monitor

--- src/monitor.ts
+++ src/monitor.ts
@@ -1,66 +1,38 @@
 /* 页面监控 */
 // reference: https://github.com/forthealllight/blog/issues/38
 
-/* addEvent('load', (ev) => {
-  console.log('load ...', performance.timing.loadEventEnd, performance.timing.navigationStart);
-  performanceMonitor();
-});
+(() => {
+  addEvent('load', (ev) => {
+    console.group('load');
+    console.info(performanceMonitor());
+    console.groupEnd();
+  });
+  addEvent('DOMContentLoaded', (ev) => {
+    console.group('DOMContentLoaded');
+    console.info(performanceMonitor());
+    console.groupEnd();
+  });
+  ws('pushState', () => {
+    console.group('pushState');
+    console.info(performanceMonitor());
+    console.groupEnd();
+  });
+})();
 
-
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('3 seconds passed');
-  performanceMonitor();
-});
-ws('pushState');
-
-function addEvent(type: string, callback: (ev: any) => void) {
-  return window.addEventListener(type, callback);
-}
-
-function ws(type: string) {
+// TODO: 根据 业务id / 页面id 来进行监控 pushState 单页监控处理
+function ws(type: string, cb: () => void) {
   const history: any = window.history;
   const originTypeOfHistory = history[type];
   history[type] = function() {
-    performanceMonitor();
+    setTimeout(cb);
     return originTypeOfHistory.bind(this, arguments);
   };
 }
 
-function performanceMonitor() {
-  const timing = performance.timing;
-  const start = timing.navigationStart;
-  let dnsTime = 0;
-  let tcpTime = 0;
-  let firstPaintTime = 0;
-  let domRenderTime = 0;
-  let loadTime = 0;
-
-  dnsTime = timing.domainLookupEnd - timing.domainLookupStart;
-  tcpTime = timing.connectEnd - timing.connectStart;
-  firstPaintTime = timing.responseStart - start;
-  domRenderTime = timing.domContentLoadedEventEnd - start;
-  loadTime = timing.loadEventEnd - start;
-
-  console.log(`
-    \nDNS解析时间: ${dnsTime / 1000} s
-    \nTCP建立时间: ${tcpTime / 1000} s
-    \n首屏时间: ${firstPaintTime / 1000} s
-    \ndom渲染完成时间: ${domRenderTime / 1000} s
-    \n页面onload时间: ${loadTime / 1000} s
-  `);
-}
- */
-
-addEvent('load', (ev) => {
-  console.log('load ...', performance.timing.loadEventEnd, performance.timing.navigationStart);
-  console.log(performanceMonitor());
-});
-
 function addEvent(type: string, callback: (ev: any) => void) {
   return window.addEventListener(type, callback);
 }
 
-// https://stackoverflow.com/questions/7606972/measuring-site-load-times-via-performance-api
 function performanceMonitor() {
   if (!window.performance || !performance.timing) {
     return 'not support the performance, please supplement it ...';
@@ -70,7 +42,7 @@ function performanceMonitor() {
   let str = '';
   let len = 0;
   let mergeList = [];
-  // const report = performance.navigation.type;
+  const report = performance.navigation.type;
   let performanceTimes = [
     'navigationStart',
     'unloadEventStart',
@@ -102,7 +74,7 @@ function performanceMonitor() {
         }
         return [proto, timestamp, timestamp - first];
       } else {
-        return [1, NaN];
+        return [proto, NaN];
       }
     })
     .filter((item: any[]) => !isNaN(item[1]));
@@ -112,21 +84,23 @@ function performanceMonitor() {
   // if (report === 1) {
   //   return performanceTimes;
   // }
-  
+
   len = performanceTimes.length - 1;
   mergeList = performanceTimes.map((item, i) => {
-    if (i > 0 && performanceTimes[i - 1][2] !== item[2]) {
-      item[0] = '\n' + item[0];
-    }
-    if (i === 0 || i === len) {
-      item[0] = '\n' + '(ts:' + item[1] + ')' + '\n' + item[0];
+    if (item[2] > 100) {
       (item[2] as number) /= 1000;
-      item[2] += ' seconds elapsed \n';
+      item[2] += ' s';
+    } else {
+      item[2] += ' ms';
     }
 
-    return item[0] + ' = ' + item[2];
+    if (i === 0 || i === len) {
+      item[0] = '(ts:' + item[1] + ')' + '\n' + item[0];
+    }
+
+    return '\n' + item[0] + ' = ' + item[2];
   });
 
-  str = '\n' + location.href + '\n' + new Date().toLocaleString();
+  str = '\n' + location.href + '\n' + new Date().toLocaleString() + '\n';
   return str + mergeList.join('\n');
 }
